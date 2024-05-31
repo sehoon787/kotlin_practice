a12144c6fb35d05b091734c5613657f547057923
sehoon.k - desktop <sehoon787@naver.com>
2024-05-31 16:55:39 +0900

    [240530] GpsMap - API 키 제외

--- GpsMap/app/src/main/java/com/example/gpsmap/MapsActivity.kt
+++ GpsMap/app/src/main/java/com/example/gpsmap/MapsActivity.kt
@@ -1,22 +1,111 @@
 package com.example.gpsmap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
-
+import android.util.Log
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.example.gpsmap.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.gpsmap.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.PolylineOptions
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
+    private val fusedLocationProviderClient by lazy { FusedLocationProviderClient(this) }
+
+    /// ################################# 권한 요청 #################################
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) { addLocationListener() }
+            else { Toast.makeText(this, "권한이 거부되었습니다", Toast.LENGTH_SHORT).show() }
+        }
+
+    private fun showPermissionInfoDialog() {
+        // 다이얼로그에 권한이 필요한 이유를 설명
+        AlertDialog.Builder(this).apply {
+            setTitle("권한이 필요한 이유")
+            setMessage("지도에 위치를 표시하려면 위치 정보 권한이 필요합니다.")
+            setPositiveButton("권한 요청") { _, _ ->
+                // 권한 요청
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+            setNegativeButton("거부", null)
+        }.show()
+    }
+
+    private fun checkPermission(cancel: () -> Unit, ok: () -> Unit){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.ACCESS_FINE_LOCATION)){ cancel() }
+            else{
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                return
+            }
+            ok()
+        }
+    }
+    /// ################################# 권한 요청 #################################
+
+    /// 이동 경로 그리기
+    private val polylineOptions = PolylineOptions().width(5f).color(Color.RED)
+
+    private val locationRequest by lazy {
+        LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 10000
+            fastestInterval = 5000
+        }
+    }
+
+    private val locationCallback = MyLocationCallBack()
+
+    @SuppressLint("MissingPermission")
+    private fun addLocationListener(){
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
+    }
+
+    private fun removeLocationListener(){
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -27,6 +116,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
+    override fun onPause() {
+        super.onPause()
+        removeLocationListener()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        checkPermission(
+            cancel = { showPermissionInfoDialog() },
+            ok = { addLocationListener() }
+        )
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -34,5 +137,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    /// ################################# inner class #################################
+    inner class MyLocationCallBack : LocationCallback(){
+        override fun onLocationResult(locationResult: LocationResult?) {
+            super.onLocationResult(locationResult)
+
+            val location = locationResult?.lastLocation
+
+            /// 현재 위치로 이동
+            location?.run {
+                val latLng = LatLng(latitude, longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
+
+                Log.d("MapsActivity", "위도: $latitude 경도: $longitude")
+
+                /// 이동 경로 그리기
+                polylineOptions.add(latLng)
+                mMap.addPolyline(polylineOptions)
+            }
+        }
     }
 }
