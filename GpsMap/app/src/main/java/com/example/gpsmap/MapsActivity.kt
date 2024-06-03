48eb9b4d9169cce126016c50efb50b888a0b998f
sehoon.k - desktop <sehoon787@naver.com>
2024-06-03 10:31:52 +0900

    [240603] GpsMap - marker update 기능 추가

--- GpsMap/app/src/main/java/com/example/gpsmap/MapsActivity.kt
+++ GpsMap/app/src/main/java/com/example/gpsmap/MapsActivity.kt
@@ -23,12 +23,14 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
+    private lateinit var mMarker: Marker
     private lateinit var binding: ActivityMapsBinding
 
     private val fusedLocationProviderClient by lazy { FusedLocationProviderClient(this) }
@@ -58,20 +60,16 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             if(ActivityCompat.shouldShowRequestPermissionRationale(
                     this, Manifest.permission.ACCESS_FINE_LOCATION)){ cancel() }
             else{
-                requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
                 requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-                return
             }
-            ok()
+            return
         }
+        ok()
     }
     /// ################################# 권한 요청 #################################
 
@@ -135,7 +133,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMarker = mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))!!
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
@@ -149,6 +147,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             /// 현재 위치로 이동
             location?.run {
                 val latLng = LatLng(latitude, longitude)
+                mMarker.position = latLng
                 mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
 
                 Log.d("MapsActivity", "위도: $latitude 경도: $longitude")
