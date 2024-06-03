c1cd2b989041fbfabfc8e148f7619f6544b70f5d
sehoon.k - desktop <sehoon787@naver.com>
2024-06-03 15:32:07 +0900

    [240603] Xylophone

--- Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
+++ Xylophone/app/src/main/java/com/example/xylophone/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.xylophone
 
+import android.media.SoundPool
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.xylophone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +11,40 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val soundPool = SoundPool.Builder().setMaxStreams(8).build()
+
+    private val sounds by lazy {
+        listOf(
+            Pair(binding.do1, R.raw.do1),
+            Pair(binding.re, R.raw.re),
+            Pair(binding.mi, R.raw.mi),
+            Pair(binding.fa, R.raw.fa),
+            Pair(binding.sol, R.raw.sol),
+            Pair(binding.la, R.raw.la),
+            Pair(binding.si, R.raw.si),
+            Pair(binding.do2, R.raw.do2),
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Manifest에서 정의함
+        // requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        sounds.forEach{ tune(it) }
+    }
+
+    private fun tune(pitch: Pair<TextView, Int>){
+        val soundId = soundPool.load(this, pitch.second, 1)
+        pitch.first.setOnClickListener {
+            soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 1.0f)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        soundPool.release()
     }
 }
