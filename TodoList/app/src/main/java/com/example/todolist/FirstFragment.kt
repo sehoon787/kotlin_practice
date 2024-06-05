448f111664e68c192845c5146ede5b9e3d4ef237
sehoon.k - desktop <sehoon787@naver.com>
2024-06-05 13:31:19 +0900

    [240603] TodoList_WIP

--- TodoList/app/src/main/java/com/example/todolist/FirstFragment.kt
+++ TodoList/app/src/main/java/com/example/todolist/FirstFragment.kt
@@ -1,22 +1,27 @@
 package com.example.todolist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.adapter.TodoListAdapter
 import com.example.todolist.databinding.FragmentFirstBinding
+import kotlinx.coroutines.launch
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+
 class FirstFragment : Fragment() {
+    private val viewModel by activityViewModels<MainViewModel>()
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -31,6 +36,22 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val todoListAdapter = TodoListAdapter{ todo ->
+            viewModel.selectedTodo = todo
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.items.collect{
+                    Log.d("FirstFragment", it.toString())
+                    todoListAdapter.submitList(it)
+                }
+            }
+        }
 
         binding.addFab.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
